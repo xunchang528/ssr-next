edaf6aab6b07b7c8179467c382166bf580d64515
xunchang528 <101688822+xunchang528@users.noreply.github.com>
2023-02-08 16:19:07 +0800

    利用中间件 next-connect

--- pages/api/layout.ts
+++ pages/api/layout.ts
@@ -3,47 +3,47 @@ import axios from "axios"
 import { ILayoutProps } from "../../components/layout";
 import { CMSDOMAIN } from "../../utils";
 import { isEmpty } from "lodash";
+import nextConnect from "next-connect";
 
-const getLayoutData = (
-  req: NextApiRequest,
-  res: NextApiResponse<ILayoutProps>
-) => {
-  axios.get(`${CMSDOMAIN}/api/layouts`).then((result) => {
-    const {
-      copy_right,
-      link_lists,
-      public_number,
-      qr_code,
-      qr_code_image,
-      site_number,
-      title,
-    } = result.data || {};
-
-    res.status(200).json({
-      navbarData: {},
-      footerData: {
+const getLayoutData = nextConnect()
+  // .use(any middleware)
+  .get((req: NextApiRequest, res: NextApiResponse<ILayoutProps>) => {
+    axios.get(`${CMSDOMAIN}/api/layouts`).then((result) => {
+      const {
+        copy_right,
+        link_lists,
+        public_number,
+        qr_code,
+        qr_code_image,
+        site_number,
         title,
-        linkList: link_lists?.data?.map((item: any) => {
-          return {
-            title: item.title,
-            list: item?.links?.data?.map((_item: any) => {
-              return {
-                label: _item.label,
-                link: isEmpty(_item.link) ? "" : _item.link,
-              };
-            }),
-          };
-        }),
-        qrCode: {
-          image: `${CMSDOMAIN}${qr_code_image.data.url}`,
-          text: qr_code,
+      } = result.data || {};
+
+      res.status(200).json({
+        navbarData: {},
+        footerData: {
+          title,
+          linkList: link_lists?.data?.map((item: any) => {
+            return {
+              title: item.title,
+              list: item?.links?.data?.map((_item: any) => {
+                return {
+                  label: _item.label,
+                  link: isEmpty(_item.link) ? "" : _item.link,
+                };
+              }),
+            };
+          }),
+          qrCode: {
+            image: `${CMSDOMAIN}${qr_code_image.data.url}`,
+            text: qr_code,
+          },
+          copyRight: copy_right,
+          siteNumber: site_number,
+          publicNumber: public_number,
         },
-        copyRight: copy_right,
-        siteNumber: site_number,
-        publicNumber: public_number,
-      },
+      });
     });
   });
-};
 
 export default getLayoutData;
