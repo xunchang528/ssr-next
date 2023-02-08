a479c5270149bf79d53a489927811fb30708a956
xunchang528 <101688822+xunchang528@users.noreply.github.com>
2023-02-08 17:29:09 +0800

    主题化
    没成功弄完

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,10 @@
-import "../styles/globals.css";
 import type { AppProps, AppContext } from "next/app";
 import App from "next/app";
-import { Layout, ILayoutProps } from "components/layout";
+import { Layout, ILayoutProps } from "@/components/layout";
 import Head from "next/head";
 import axios from "axios";
-import { LOCALDOMAIN } from "utils";
+import { LOCALDOMAIN } from "@/utils";
+import { ThemeContextProvider } from "@/stores/theme";
 import "./global.scss";
 
 const MyApp = (data: AppProps & ILayoutProps) => {
@@ -13,16 +13,18 @@ const MyApp = (data: AppProps & ILayoutProps) => {
   return (
     <div>
       <Head>
-        <title>稀土掘金</title>
+        <title>A Demo for 《深入浅出SSR官网开发指南》</title>
         <meta
           name="description"
           content="A Demo for 《深入浅出SSR官网开发指南》"
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Layout navbarData={navbarData} footerData={footerData}>
-        <Component {...pageProps} />
-      </Layout>
+      <ThemeContextProvider>
+        <Layout navbarData={navbarData} footerData={footerData}>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeContextProvider>
     </div>
   );
 };
