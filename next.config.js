64951f074158f4da4fe39b44d35c08479aa098ae
xunchang528 <101688822+xunchang528@users.noreply.github.com>
2023-02-08 16:07:19 +0800

    BBF接口定义

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol:'http',
+        hostname:'127.0.0.1',
+        port:'1337',
+        pathname:'/uploads/code_67191e1aba.png'
+      }
+    ]
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
